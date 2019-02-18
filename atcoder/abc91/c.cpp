0dfcbe883dfd72e8b86d7ed55d66a2f2fbfd8d5b
woody <piequalsabout314159@gmail.com>
2019-02-18 13:36:48 -0500

    solved abc91c

--- atcoder/abc91/c.cpp
+++ atcoder/abc91/c.cpp
@@ -1,3 +1,4 @@
+//!! unable to solve, read editorial
 #include <bits/stdc++.h>
 #define pq priority_queue
 #define mp make_pair
@@ -29,12 +30,24 @@ int dy[4] = {0, 1, 0, -1};
 
 using namespace std;
 
+template <class T = int> T in() {
+    T x;
+    cin >> x;
+    return (x);
+}
+
+template <class T> void print(T &x) {
+    cout << x << '\n';
+    return;
+}
 typedef long long int64;
 typedef long long lint;
 typedef long long lli;
 typedef long long ll;
 typedef unsigned int uint;
 typedef unsigned long long ull;
+typedef pair<int, int> pint;
+typedef pair<long, long> plong;
 typedef pair<int, int> pii;
 typedef pair<ll, int> pli;
 typedef pair<int, ll> pil;
@@ -43,6 +56,28 @@ typedef pair<ll, ll> pll;
 const int iinf = 1 << 29;
 const long long linf = 1ll << 61;
 
+int n;
+pii red[103], blue[103];
+bool usedred[103], usedblue[103];
 int main() {
+    cin >> n;
+    REP(i, n) { cin >> red[i].first >> red[i].second; }
+    REP(i, n) { cin >> blue[i].second >> blue[i].first; }
+    sort(red, red + n, greater<pint>());
+    sort(blue, blue + n);
+
+    int ans = 0;
+    REP(i, n) {
+        REP(j, n) {
+            if (red[i].first < blue[j].second &&
+                red[i].second < blue[j].first && !usedred[i] && !usedblue[j]) {
+                ans++;
+                usedred[i] = 1;
+                usedblue[j] = 1;
+                break;
+            }
+        }
+    }
+    print(ans);
     return 0;
 }
