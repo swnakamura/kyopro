856100848275ea1bb476b27d2ba836f91c6acd33
woody <piequalsabout314159@gmail.com>
2019-02-18 16:03:14 -0500

    bug fixed.

--- atcoder/abc90/c.cpp
+++ atcoder/abc90/c.cpp
@@ -58,10 +58,20 @@ const int iinf = 1 << 29;
 const long long linf = 1ll << 61;
 
 int main() {
-    ll n,m;
-    cin>>n>>m;
-    if(n==1){cout<<m-2<<endl;return 0;}
-    if(m==1){cout<<n-2<<endl;return 0;}
-    cout<<(n-2)*(m-2)<<endl;
+    ll n, m;
+    cin >> n >> m;
+    if (n == 1 && m == 1) {
+        cout << 1 << endl;
+        return 0;
+    }
+    if (n == 1) {
+        cout << m - 2 << endl;
+        return 0;
+    }
+    if (m == 1) {
+        cout << n - 2 << endl;
+        return 0;
+    }
+    cout << (n - 2) * (m - 2) << endl;
     return 0;
 }
